9eb517eac46ba2fa331d102ab4bf5051bc225107
jy3026 <jy_3026@naver.com>
2022-07-24 23:39:22 +0900

    4주차
    4주차

---
+++ 4week/day24/Test.java
@@ -0,0 +1,15 @@
+package day1;
+
+import java.util.Scanner;
+
+
+public class Test {
+	public long sum(int[] a) {
+		long result=0;
+		for(int i=0;i<a.length;i++) {
+			result+=a[i];
+		}
+		return result;
+	}
+}
+
